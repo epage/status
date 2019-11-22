c374a42365892b3b6c86a705189444a82a11951f
Ed Page <epage@duosecurity.com>
2019-11-22 10:15:53 -0700

    fix: Ensure we have a std feature

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ mod context;
 mod ext;
 mod kind;
 
+#[cfg(not(feature = "std"))]
+compile_error!("no_std support is not implemented yet");
+
 pub use crate::alarm::*;
 pub use crate::context::*;
 pub use crate::ext::*;
